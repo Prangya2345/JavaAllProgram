3ccc7499c11673324fd615ee97c8cf770e407499
Prangya2345 <142502978+Prangya2345@users.noreply.github.com>
2025-06-18 20:54:26 +0530

    Update SumOfEvenNumbersInList.java

--- HelloWorldProject/src/test/java/PracticeAllProject/SumOfEvenNumbersInList.java
+++ HelloWorldProject/src/test/java/PracticeAllProject/SumOfEvenNumbersInList.java
@@ -4,16 +4,15 @@ import java.util.List;
 public class SumOfEvenNumbersInList {
 
 	public static void main(String[] args) {
-		List<Integer> numbers = Arrays.asList(5, 2, 8, 7, 4, 3, 10);
+        List<Integer> numbers = Arrays.asList(5, 2, 8, 7, 4, 3, 10);
+        int sum = 0;
 
-        int sum = numbers.stream()
-                         .filter(n -> n % 2 == 0)
-                         .mapToInt(Integer::intValue)
-                         .sum(); // Sum them
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                sum += num;
+            }
+        }
 
         System.out.println("Sum of even numbers: " + sum);
     }
-
-
-
 }
