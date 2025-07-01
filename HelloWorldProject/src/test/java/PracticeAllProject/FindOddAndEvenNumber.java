d731f79bd003c23ccd7500d1f2822cfafbb18d6f
Prangya2345 <142502978+Prangya2345@users.noreply.github.com>
2025-07-01 20:17:58 +0530

    Create FindOddAndEvenNumber.java

---
+++ HelloWorldProject/src/test/java/PracticeAllProject/FindOddAndEvenNumber.java
@@ -0,0 +1,18 @@
+package PracticeAllProject;
+
+public class FindOddAndEvenNumber {
+
+	
+		public static void main(String[] args) {
+	        int number = 8;
+
+	        if (number % 2 == 0) {
+	            System.out.println(number + " is Even.");
+	        } else {
+	            System.out.println(number + " is Odd.");
+	        }
+	    }
+
+	}
+
+
