1e59aea06865f6eeacc3d48dcd0ae92bad853eef
Prangya2345 <142502978+Prangya2345@users.noreply.github.com>
2025-07-01 22:03:58 +0530

    Update FibonacciSeriesNumber.java

--- HelloWorldProject/src/test/java/PracticeAllProject/FibonacciSeriesNumber.java
+++ HelloWorldProject/src/test/java/PracticeAllProject/FibonacciSeriesNumber.java
@@ -5,18 +5,15 @@ public class FibonacciSeriesNumber {
 	public static void main(String[] args) {
 		int num = 3553, reversedNum = 0, remainder;
 	    
-	    // store the number to originalNum
-	    int originalNum = num;
+		int originalNum = num;
 	    
-	    // get the reverse of originalNum
-	    // store it in variable
 	    while (num != 0) {
 	      remainder = num % 10;
 	      reversedNum = reversedNum * 10 + remainder;
 	      num /= 10;
 	    }
 	    
-	    // check if reversedNum and originalNum are equal
+	   
 	    if (originalNum == reversedNum) {
 	      System.out.println(originalNum + " is Palindrome.");
 	    }
